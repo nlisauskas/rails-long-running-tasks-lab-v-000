bc72bc1c50e5afd19e94f306590aba3225f96cad
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-30 03:09:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,54 +1,16 @@
 class SongsController < ApplicationController
 
+  require 'csv'
+
   def index
-    @songs = Song.all
+    @customers = Customer.all
   end
 
-  def show
-    @song = Song.find(params[:id])
-  end
-
-  def new
-    @song = Song.new
-  end
-
-  def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
+  def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
+    redirect_to customers_path
   end
-
-  def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
-
-  private
-
-  def song_params
-    params.require(:song).permit(:title, :artist_name)
-  end
+  
 end
-
